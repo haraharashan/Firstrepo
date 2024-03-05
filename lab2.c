406c9337f6c3a87126774463cc4329a8085e1930
Haralambie Mihai <adresa@gmail.com>
2024-03-05 14:46:16 +0200

    First commit

--- lab2.c
+++ lab2.c
@@ -2,19 +2,18 @@
 #include <stdio.h>
 
 int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+    if(x==0)return x;
+    x=x-2;
+    x=function(x);
 }
 
 int main(){
 	int a=30, i=0;
-	
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
